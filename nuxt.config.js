b1ca2c5069d44a908f224c4cce0d77b31bd09938
khwan <rudghks7816@gmail.com>
2020-01-19 13:40:40 +0900

    feat: seo 데이터 수정
    * html lang 수정 en > ko
    * head meta : keywords 추가

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,11 +6,15 @@ export default {
   ** Headers of the page
   */
   head: {
+    htmlAttrs: {
+      lang: 'ko'
+    },
     title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { hid: 'keywords', name: 'keywords', content: '백색소음, nuxt, vue, khwan' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
